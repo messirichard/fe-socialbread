3f8743cdd29589a13ff66cb9ea4abcd61812b094
Messiah Richardo F <richardofay@gmail.com>
2021-08-12 03:14:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary:  '#05433B'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
